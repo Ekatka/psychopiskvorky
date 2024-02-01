ce24d00dfb8a27b18655da9b02a390d8cdb2d03e
Ekatka <katya.danilina@gmail.com>
2024-02-01 13:04:26 +0100

    add reset after full grid

--- main.py
+++ main.py
@@ -15,16 +15,15 @@ class Playground(arcade.Window):
         self.player = player
         self.cell_size = cell_size
         self.grid_size = grid_size
-        self.window_size = self.grid_size * self.cell_size + 100
+        self.window_size = self.grid_size * self.cell_size + grid_offset
         super().__init__(self.window_size, self.window_size, title)
         arcade.set_background_color(arcade.color.WHITE)
         self.game = game
-        self.move = [] #jeden tah
+        self.move = []  # jeden tah
         self.n = network
-        self.state = 0 #stav hry, podle něj se vypisuje text pod hracím oknem
+        self.state = 0  # stav hry, podle něj se vypisuje text pod hracím oknem
         self.grid_offset = grid_offset
         self.waiting = False
-
 
     def get_cell_size(self):
         return int(self.cell_size)
@@ -34,6 +33,7 @@ class Playground(arcade.Window):
 
     def get_window_size(self):
         return int(self.window_size)
+
     def get_grid_offset(self):
         return int(self.grid_offset)
 
@@ -50,26 +50,28 @@ class Playground(arcade.Window):
     def on_mouse_press(self, x: int, y: int, button: int, modifiers: int):
 
         if self.game.gameOver:
-            #dohrálo se a po kliknutí začne nová hra
+            # dohrálo se a po kliknutí začne nová hra
             self.n.send('resetGame')
 
         else:
-            #waiting blokuje vybrání jiného políčka po zahrání
+            # waiting blokuje vybrání jiného políčka po zahrání
             if self.game.guessMove == [0, 0] or self.game.guessMove[not self.player] != 0:
                 self.waiting = False
-            #souřadnice se upraví a převedou se na souřadnice v tabulce
+            # souřadnice se upraví a převedou se na souřadnice v tabulce
             if not self.waiting and self.game.ready:
                 cell_x = (x - self.grid_offset // 2) // self.cell_size
                 cell_y = (y - self.grid_offset) // self.cell_size
-                #kontroluju, že dané políčko je prázdné a je v rozshahu tabulky
+                # kontroluju, že dané políčko je prázdné a je v rozshahu tabulky
                 if not [cell_x, cell_y] in self.game.fullMoves[0] or [cell_x, cell_y] in self.game.fullMoves[1]:
                     if cell_x >= 0 and cell_x < 25 and cell_y >= 0 and cell_y < 25:
                         self.move = [cell_x, cell_y]
                         self.game = self.n.send(self.move)
-                        #když si vybrali oba, vynuluju kroky
+                        # když si vybrali oba, vynuluju kroky
                         if self.game.bothChose():
                             self.waiting = False
                             self.n.send('resetWent')
+
+
 
     def update(self, delta_time: float):
         self.game = self.n.send('get')
@@ -78,20 +80,24 @@ class Playground(arcade.Window):
     def manage_states(self):
         turn = self.game.getTurn()
         player = self.player
-        #nejsou připojeni oba hráči, ale hra už začala
-        if not self.game.ready and self.game.gameOver:
+        #všechna políčka jsou zaplněná
+        if len(self.game.fullMoves[0]) + len(self.game.fullMoves[1]) == self.grid_size ** 2:
+            self.state = 10
+            self.game.gameOver = True
+        # nejsou připojeni oba hráči, ale hra už začala
+        elif not self.game.ready and self.game.gameOver:
             self.state = 9
-        #nejsou připojeni oba hřáči
+        # nejsou připojeni oba hřáči
         elif not self.game.ready:
             self.state = 7
-        #jeden z hráčů vyhrál
+        # jeden z hráčů vyhrál
         elif self.game.gameOver == True and self.game.ready:
             self.state = 8
 
         else:
 
             if turn == player:
-                #možné stavy toho, kdo už odehrál, podle nich se pak generuje text
+                # možné stavy toho, kdo už odehrál, podle nich se pak generuje text
                 if self.game.guessMove[player] == 0 and self.game.guessMove[int(not player)] == 0:
                     self.state = 1
                 elif self.game.guessMove[player] == 0 and self.game.guessMove[int(not player)] != 0:
@@ -107,9 +113,10 @@ class Playground(arcade.Window):
                 elif self.game.guessMove[player] != 0 and self.game.guessMove[int(not player)] == 0:
                     self.state = 6
 
+
 def draw_grid(game, playground, state):
     grid_offset = playground.get_grid_offset()
-    #kreslí se tabulka
+    # kreslí se tabulka
     for x in range(playground.get_grid_size() + 1):
         start_x = x * playground.get_cell_size() + grid_offset // 2
         end_x = x * playground.get_cell_size() + grid_offset // 2
@@ -124,7 +131,7 @@ def draw_grid(game, playground, state):
         end_y = start_y
         arcade.draw_line(start_x, start_y, end_x, end_y, arcade.color.BLACK)
 
-    #zakreslují se již umístěné symboly
+    # zakreslují se již umístěné symboly
     for symbol in game.fullMoves[0]:
         x_cor = symbol[0] * playground.get_cell_size() + playground.get_cell_size() // 2 + grid_offset // 2
         y_cor = symbol[1] * playground.get_cell_size() + playground.get_cell_size() // 2 + grid_offset
@@ -133,13 +140,13 @@ def draw_grid(game, playground, state):
         x_cor = symbol[0] * playground.get_cell_size() + playground.get_cell_size() // 2 + grid_offset // 2
         y_cor = symbol[1] * playground.get_cell_size() + playground.get_cell_size() // 2 + grid_offset
         arcade.draw_text('X', x_cor, y_cor, arcade.color.BLACK, font_size=20, anchor_x="center", anchor_y="center")
-    #ukazuje se, kam posledně hádal soupeř
+    # ukazuje se, kam posledně hádal soupeř
     if game.wrongGuess != 0:
         x_cor = game.wrongGuess[0] * playground.get_cell_size() + playground.get_cell_size() // 2 + grid_offset // 2
         y_cor = game.wrongGuess[1] * playground.get_cell_size() + playground.get_cell_size() // 2 + grid_offset + 22
         arcade.draw_text('.', x_cor, y_cor, arcade.color.RED, font_size=50, anchor_x='center', anchor_y="center")
 
-    #podle stavů se vypisuje text
+    # podle stavů se vypisuje text
     if state == 0:
         text = 'Initializing'
     elif state == 1:
@@ -158,6 +165,8 @@ def draw_grid(game, playground, state):
         text = 'Waiting for the opponent to connect'
     elif state == 9:
         text = 'Opponent disconnected, game over'
+    elif state == 10:
+        text = 'Game over, no one won'
 
     if state != 8:
         text_x = playground.width / 2
@@ -176,17 +185,18 @@ def draw_grid(game, playground, state):
 
 def draw_end_game(game, playground):
     grid_offset = playground.get_grid_offset()
-    #nakreslím tabulku
+    # nakreslím tabulku
     draw_grid(game, playground, state=8)
 
-    #najdu si souřadnice uloženého políčka
+    # najdu si souřadnice uloženého políčka
     x = game.winningPlacement[0]
     y = game.winningPlacement[1]
-    #najdu si sousední políčko
+    # najdu si sousední políčko
     cur_x = x + game.winningDirection[0]
     cur_y = y + game.winningDirection[1]
 
-    #procházím, dokud jsou políčka v seznamu zahraných, těma pak vedu červenou úsečku
+    # procházím, dokud jsou políčka v seznamu zahraných, těma pak vedu červenou úsečku
+    # x a y jsou souřadnice krajního políčka, to znamená, že když se budu pohybovat ve správném směru, najdu všechna
     if [cur_x, cur_y] in game.fullMoves[game.winner]:
         while [cur_x, cur_y] in game.fullMoves[game.winner]:
             cur_x = cur_x + game.winningDirection[0]
@@ -197,14 +207,15 @@ def draw_end_game(game, playground):
         start_y = ((y + game.winningDirection[1]) * playground.get_cell_size()
                    + playground.get_cell_size() // 2 + grid_offset)
         end_x = ((cur_x - game.winningDirection[0]) * playground.get_cell_size()
-                   + playground.get_cell_size() // 2 + grid_offset // 2)
+                 + playground.get_cell_size() // 2 + grid_offset // 2)
         end_y = ((cur_y - game.winningDirection[1]) * playground.get_cell_size()
-                   + playground.get_cell_size() // 2 + grid_offset)
+                 + playground.get_cell_size() // 2 + grid_offset)
         arcade.draw_line(start_x, start_y, end_x, end_y, color=arcade.color.RED, line_width=3)
+
 
 def main():
     n = Network()
-    #přiradím hráči číslo
+    # přiradím hráči číslo
     player = n.getNumOfP()
     try:
         game = n.send('get')
@@ -215,7 +226,6 @@ def main():
 
     playground = Playground(player, title=f'You are a player {player}', game=game, network=n)
     arcade.run()
-
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -3,10 +3,7 @@ from _thread import *
 import pickle
 from game import Game
 
-
-
-#uživatel zadá svoji IP adresu, pak se čeká na připojení
-
+# uživatel zadá svoji IP adresu, pak se čeká na připojení
 server = input('Enter your IP address')
 port = 5555
 
@@ -18,30 +15,29 @@ except socket.error as e:
     print(e)
     exit()
 
-
-
 print('Server is running, waiting for connection')
+
 
 def ThreadedClient(conn, numOfPlayer, game):
     conn.send(str.encode(str(numOfPlayer)))
     while True:
         try:
-            #pro každého z klientu kontrolujeme data
+            # pro každého z klientu kontrolujeme data
             data = conn.recv(2048).decode()
             if not data:
                 break
             else:
-                 #kontrolujeme obsah dat, připadně provedeme přikazy
+                # kontrolujeme obsah dat, připadně provedeme přikazy
                 if data == 'resetWent':
                     game.resetWent()
                 elif data == 'resetGame':
                     game.resetGame()
-                #když data nejsou get ani jiný z příkazů, tak to musí být zahraný krok
+                # když data nejsou get ani jiný z příkazů, tak to musí být zahraný krok
                 elif data != 'get':
                     data = data.split(' ')
                     data = [int(i) for i in data]
                     game.play(numOfPlayer, data)
-                #vrátíme současnou hru
+                # vrátíme současnou hru
                 conn.sendall(pickle.dumps(game))
 
 
@@ -53,14 +49,14 @@ def ThreadedClient(conn, numOfPlayer, game):
     conn.sendall(pickle.dumps(game))
     conn.close()
 
+
 s.listen(2)
 numOfPlayers = 0
 while True:
-    #Čekáme na dvě připojení
+    # Čekáme na dvě připojení
     if numOfPlayers < 2:
         conn, addr = s.accept()
         print("Connected to:", addr)
-
 
         if numOfPlayers == 0:
             numOfPlayers += 1
@@ -68,14 +64,6 @@ while True:
 
         else:
             game.ready = True
-            numOfPlayers +=1
+            numOfPlayers += 1
 
-
-        start_new_thread(ThreadedClient, (conn, numOfPlayers-1, game))
-
-
-
-
-
-
-
+        start_new_thread(ThreadedClient, (conn, numOfPlayers - 1, game))
